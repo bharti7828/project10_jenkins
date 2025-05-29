b1527bbafff677d27164f28f620536d9b120f978
bharti7828 <bhartijaypale@gmail.com>
2025-05-29 12:35:30 +0530

    ok

---
+++ projectORS/src/main/java/com/rays/dao/TimeTableDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.TimeTableDTO;
+/**
+ * Bharti Jaypal 
+ *
+ */
+public interface TimeTableDAOInt extends BaseDAOInt<TimeTableDTO> {
+
+	
+}
